cab616eff2c9b596da5e02ecf954bd489fe16ff1
Ali-Hassan-Ali <alihassanalimadny@gmail.com>
2022-09-21 17:28:27 +0200

    first commit

---
+++ database/migrations/2022_09_19_171857_create_apartments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApartmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apartments', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('location');
+
+            $table->foreignIdFor(\App\Models\Banner::class)->nullable();
+            $table->foreignIdFor(\App\Models\ProductCategory::class)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apartments');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Http;
+// use Spatie\Browsershot\Browsershot;
+use App\Models\Order;
+use App\Models\PaymentOrder;
+use App\Models\User;
+use App\Models\Admin;
+
+// Route::get('/', [\App\Http\Controllers\Api\WelcomeController::class,'index']);
+
+
+Route::get('/', function () {
+
+    $order = Order::first();
+
+    // dd(auth('admin')->user()->isAbleTo('admins-create'));
+    // return $order;
+    $user  = User::find($order->user_id);
+    // dd($orders);
+
+    // dd($orders);
+
+    return view('dashboard_admin.invoice.order', compact('order', 'user'));
+
+    // Browsershot::url('https://example.com')->save('example.pdf');
+
+ 
+});
+
+
+Route::get('test', function () {
+
+    \PDF::loadView('home')
+    ->setPaper('letter', 'landscape')
+    ->save(public_path('invoice.pdf'));
+
+    $banners = App\Models\Banner::where('categoreys_id',1)->get();
+
+    return response()->api(App\Http\Resources\BannerResource::collection($banners));
+
+});
+
+Auth::routes();
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/imei', [App\Http\Controllers\HomeController::class, 'imeiCheck'])->name('imei');
+Route::post('/imei', [App\Http\Controllers\HomeController::class, 'imeiSubmit']);
